de76d09b4ab1ed28f68e19bd309a3e1e7c31460c
JoãoHGS <74212278+joaojhgs@users.noreply.github.com>
2022-12-07 01:29:06 +0000

    Initial layoutrendering setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ConfigProvider } from 'antd'
+import MainLayout from '../components/layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider direction="rtl" locale={{ locale: 'en' }}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ConfigProvider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,61 +5,12 @@ import {
   UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import { Calendar, Layout, Menu } from 'antd';
 import React, { useState } from 'react';
 const { Header, Sider, Content } = Layout;
 
 export default function Home() {
-  const [collapsed, setCollapsed] = useState(false);
   return (
-      <Layout className="layout">
-        <Sider trigger={null} collapsible collapsed={collapsed}>
-          <div className="logo" />
-          <Menu
-              theme="dark"
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              items={[
-                {
-                  key: '1',
-                  icon: <UserOutlined />,
-                  label: 'nav 1',
-                },
-                {
-                  key: '2',
-                  icon: <VideoCameraOutlined />,
-                  label: 'nav 2',
-                },
-                {
-                  key: '3',
-                  icon: <UploadOutlined />,
-                  label: 'nav 3',
-                },
-              ]}
-          />
-        </Sider>
-        <Layout className="site-layout">
-          <Header
-              className="site-layout-background"
-              style={{
-                padding: 0,
-              }}
-          >
-            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: () => setCollapsed(!collapsed),
-            })}
-          </Header>
-          <Content
-              className="site-layout-background"
-              style={{
-                margin: '24px 16px',
-                padding: 24,
-              }}
-          >
-            Content
-          </Content>
-        </Layout>
-      </Layout>
+    <Calendar />
   )
 }
